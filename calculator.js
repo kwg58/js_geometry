68788fd5514ea805be3e3db0357f49010dcfbbc2
Kevin Golden <kwg@cs.net>
2016-06-13 18:46:20 -0400

    complete but no bonus

--- calculator.js
+++ calculator.js
@@ -1,8 +1,26 @@
 var calculator = {
   add: function(add1, add2) {
     return add1 + add2
-  }
+  },
   subtract: function(subt1, subt2) {
     return subt1 - subt2
+  },
+  multiply: function(mult1, mult2) {
+    return mult1 * mult2;
+  },
+  divide: function(div1, div2) {
+    return div1 / div2;
+  },
+  exponent: function(exp1, exp2) {
+    return Math.pow(exp1, exp2);
+  },
+  maxi: function(m1, m2) {
+    return Math.max(m2, m2)
   }
 }
+// * Addition
+// * Subtraction
+// * Multiplication
+// * Division
+// * Exponents (Hint: Look up Javascript's Math object)
+// * An operation of your choice!
